b10ca5cf49a86462cad5033fc0da270279635dd9
radenibnu <radenibnuqc@gmail.com>
2025-11-04 10:05:44 +0700

    create Homapage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
-import React from 'react'
+'use client';
+import ExploreBtn from '../components/ExploreBtn';
+import EventCard from '@/components/EventCard';
+import { events } from '@/libs/constants';
+
 
 const Home = () => {
 
@@ -7,6 +11,21 @@ const Home = () => {
       <section>
         <h1 className='text-center'>The Hub for Every Dev <br /> Event You Can't Miss</h1>
         <p className='text-center mt-5'>Hackathons, Meetups, and Conferences, All in One Place</p>
+
+        <ExploreBtn />
+
+        <div className='mt-20 space-y-7'>
+          <h3>Featured Events</h3>
+
+          <ul className='events'>
+            {events.map((event) =>
+              <li key={event.title} className='list-none'> 
+                <EventCard {...event} />
+              </li>
+            )}
+          </ul>
+
+        </div>
       </section>
       
     </main>
---
+++ components/ExploreBtn.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from 'next/image';
+
+const ExploreBtn = () => {
+  return (
+    <button type='button' id='explore-btn' className='mt-7 mx-auto' onClick={() => console.log('CLICK')}>
+      <a href="#events">
+        Explore Events
+        <Image src="/icons/arrow-down.svg" alt="Explore Events" width={24} height={24} />
+      </a>
+    </button>
+  )
+}
+
+export default ExploreBtn
